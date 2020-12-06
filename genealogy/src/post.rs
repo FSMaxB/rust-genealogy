da36820b7e6037b3efe31e1770e5812d15794d53
Max Bruckner <max@maxbruckner.de>
2020-12-06 03:53:26 +0100

    Implement VideoSlug

--- genealogy/src/post.rs
+++ genealogy/src/post.rs
@@ -2,3 +2,4 @@ mod description;
 mod slug;
 mod tag;
 mod title;
+mod video_slug;
---
+++ genealogy/src/post/video_slug.rs
@@ -0,0 +1,30 @@
+use crate::java_replicas::exception::Exception;
+use crate::java_replicas::exception::Exception::IllegalArgument;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VideoSlug {
+	pub value: String,
+}
+
+impl VideoSlug {
+	#[allow(dead_code)]
+	pub fn from_value(value: String) -> Result<VideoSlug, Exception> {
+		if value.trim().is_empty() {
+			Err(IllegalArgument("VideoSlugs can't have an empty value.".to_string()))
+		} else {
+			Ok(VideoSlug { value })
+		}
+	}
+
+	// NOTE: compareTo is already handled by the `PartialOrd` and `Ord` derives.
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn empty_text_exception() {
+		assert!(matches!(VideoSlug::from_value("".to_string()), Err(IllegalArgument(_))))
+	}
+}

6a25af7655737f314c3d85fe6942e2b17051f955
Max Bruckner <max@maxbruckner.de>
2020-12-06 04:00:17 +0100

    Add PostTrait

--- genealogy/src/post.rs
+++ genealogy/src/post.rs
@@ -1,5 +1,20 @@
+use crate::post::description::Description;
+use crate::post::slug::Slug;
+use crate::post::tag::Tag;
+use crate::post::title::Title;
+use chrono::NaiveDate;
+use std::collections::BTreeSet;
+
 mod description;
 mod slug;
 mod tag;
 mod title;
 mod video_slug;
+
+pub trait PostTrait {
+	fn title(&self) -> &Title;
+	fn tags(&self) -> &BTreeSet<Tag>;
+	fn date(&self) -> NaiveDate;
+	fn description(&self) -> &Description;
+	fn slug(&self) -> &Slug;
+}

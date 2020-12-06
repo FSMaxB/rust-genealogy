766cefd153065e73dad951c8cfba53940b0ae658
Max Bruckner <max@maxbruckner.de>
2020-12-06 04:32:44 +0100

    Implement Post

--- genealogy/src/post.rs
+++ genealogy/src/post.rs
@@ -1,9 +1,13 @@
+use crate::post::article::Article;
 use crate::post::description::Description;
 use crate::post::slug::Slug;
 use crate::post::tag::Tag;
+use crate::post::talk::Talk;
 use crate::post::title::Title;
+use crate::post::video::Video;
 use chrono::NaiveDate;
 use std::collections::BTreeSet;
+use std::ops::Deref;
 
 mod article;
 mod content;
@@ -22,4 +26,26 @@ pub trait PostTrait {
 	fn date(&self) -> NaiveDate;
 	fn description(&self) -> &Description;
 	fn slug(&self) -> &Slug;
+}
+
+#[allow(dead_code)]
+pub enum Post {
+	Article(Article),
+	Talk(Talk),
+	Video(Video),
+}
+
+// NOTE: Although one could manually implement `PostTrait` for `Post`,
+// this is much easier to write and should work the same ergonomically when using it.
+impl Deref for Post {
+	type Target = dyn PostTrait;
+
+	fn deref(&self) -> &Self::Target {
+		use Post::*;
+		match self {
+			Article(article) => article as &dyn PostTrait,
+			Talk(talk) => talk as &dyn PostTrait,
+			Video(video) => video as &dyn PostTrait,
+		}
+	}
 }

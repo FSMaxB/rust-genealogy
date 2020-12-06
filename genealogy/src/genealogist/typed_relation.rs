643a5df5a35c17d15e0035e9496ce1167cab89de
Max Bruckner <max@maxbruckner.de>
2020-12-06 16:31:00 +0100

    TypedRelation: Use Arc<Post>
    Because we want a reference, not a complete copy of the post.

--- genealogy/src/genealogist/typed_relation.rs
+++ genealogy/src/genealogist/typed_relation.rs
@@ -2,12 +2,12 @@ use crate::genealogist::relation_type::RelationType;
 use crate::java_replicas::exception::Exception;
 use crate::java_replicas::exception::Exception::IllegalArgument;
 use crate::post::Post;
+use std::sync::Arc;
 
 #[derive(PartialEq, Eq, Hash)]
 pub struct TypedRelation {
-	// TODO: This will probably require some kind of reference type.
-	pub post1: Post,
-	pub post2: Post,
+	pub post1: Arc<Post>,
+	pub post2: Arc<Post>,
 	// NOTE: `relation_type` instead of `type` because `type` is a keyword in rust.
 	pub relation_type: RelationType,
 	pub score: u64,
@@ -15,7 +15,12 @@ pub struct TypedRelation {
 
 impl TypedRelation {
 	#[allow(dead_code)]
-	pub fn new(post1: Post, post2: Post, relation_type: RelationType, score: u64) -> Result<TypedRelation, Exception> {
+	pub fn new(
+		post1: Arc<Post>,
+		post2: Arc<Post>,
+		relation_type: RelationType,
+		score: u64,
+	) -> Result<TypedRelation, Exception> {
 		// WTF: Why use a `long` if you are actually only allowing values up to 100?
 		// RUSTIFICATION: Create a separate type that upholds this invariant.
 		if score > 100 {

12751be3b3f1078d2f3a8745b7fc619d3ed69b3c
Max Bruckner <max@maxbruckner.de>
2020-12-09 02:14:03 +0100

    Implement Genealogist for certain closures

--- genealogy/src/genealogist.rs
+++ genealogy/src/genealogist.rs
@@ -10,3 +10,13 @@ pub mod typed_relation;
 pub trait Genealogist {
 	fn infer(&self, post1: Arc<Post>, post2: Arc<Post>) -> Result<TypedRelation, Exception>;
 }
+
+// NOTE: In Java this is automatically implemented
+impl<Function> Genealogist for Function
+where
+	Function: Fn(Arc<Post>, Arc<Post>) -> Result<TypedRelation, Exception>,
+{
+	fn infer(&self, post1: Arc<Post>, post2: Arc<Post>) -> Result<TypedRelation, Exception> {
+		self(post1, post2)
+	}
+}

63b8565bf595b397ad7918bc088c44c4fd1cee71
Max Bruckner <max@maxbruckner.de>
2020-12-09 02:13:06 +0100

    Genealogist: Take Arc<Post> and allow throwing exceptions

--- genealogy/src/genealogist.rs
+++ genealogy/src/genealogist.rs
@@ -1,10 +1,12 @@
 use crate::genealogist::typed_relation::TypedRelation;
+use crate::helpers::exception::Exception;
 use crate::post::Post;
+use std::sync::Arc;
 
 mod genealogist_service;
 pub mod relation_type;
 pub mod typed_relation;
 
 pub trait Genealogist {
-	fn infer(&self, post1: Post, post2: Post) -> TypedRelation;
+	fn infer(&self, post1: Arc<Post>, post2: Arc<Post>) -> Result<TypedRelation, Exception>;
 }

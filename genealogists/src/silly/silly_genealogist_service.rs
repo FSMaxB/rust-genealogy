8a78189c4d77e9c0f7855751dfacc11baee5a643
Max Bruckner <max@maxbruckner.de>
2021-10-25 13:44:50 +0200

    Fully reimplement SillyGenealogistService.java

--- genealogists/src/silly/silly_genealogist_service.rs
+++ genealogists/src/silly/silly_genealogist_service.rs
@@ -5,10 +5,23 @@ use genealogy::helpers::exception::Exception;
 use genealogy::helpers::stream::Stream;
 use genealogy::post::Post;
 
+/// ```java
+/// public class SillyGenealogistService implements GenealogistService {
+/// ```
 pub struct SillyGenealogistService;
 
+/// ```java
+/// public class SillyGenealogistService implements GenealogistService {
+/// ```
 impl GenealogistServiceTrait for SillyGenealogistService {
-	fn procure(&self, _posts: Stream<Post>) -> Result<Genealogist, Exception> {
+	/// ```java
+	/// @Override
+	///	public Genealogist procure(Collection<Post> posts) {
+	///		return new SillyGenealogist();
+	///	}
+	/// ```
+	#[allow(unused_variables)]
+	fn procure(&self, posts: Stream<Post>) -> Result<Genealogist, Exception> {
 		Ok(SillyGenealogist::new().into())
 	}
 }
